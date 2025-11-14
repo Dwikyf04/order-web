7375c2c41ff4c8092bf76a62dc0ecbb3a206b4ac
Dwiky Fernanda <dwikyfernanda78@gmail.com>
2025-11-15 04:28:52 +0700

    Update main.jsx

--- src/main.jsx
+++ src/main.jsx
@@ -2,6 +2,7 @@ import React from "react";
 import ReactDOM from "react-dom/client";
 // 1. Ubah import ini dari App ke OrderPage
 import OrderPage from "./OrderPage"; 
+
 import "./index.css"; 
 
 ReactDOM.createRoot(document.getElementById("root")).render(
@@ -10,3 +11,4 @@ ReactDOM.createRoot(document.getElementById("root")).render(
     <OrderPage />
   </React.StrictMode>
 );
+
