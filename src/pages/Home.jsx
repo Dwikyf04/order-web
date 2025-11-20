8d5e09672a97d8a74cbf1e7e6f01a10121c6c17d
Dwiky11 <dwikyfernanda78@gmail.com>
2025-11-20 17:31:24 +0700

    menghapus logo tahu

--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -245,7 +245,6 @@ export default function Home() {
             { src: "/img/patnership/cvbbs1.jpeg", name: "CV BBS" },
             { src: "/img/patnership/mitraamanah1.jpeg", name: "Mitra Amanah" },
             { src: "/img/patnership/shoes.png", name: "Shoes" },
-            { src: "/img/patnership/TaHU.png", name: "TaHU" },
           ].map((brand, i) => (
             <div
               key={i}
