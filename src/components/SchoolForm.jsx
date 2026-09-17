9f1a980be662ce0948f2feadad7bec2cd9194aee
Dwiky11 <dwikyfernanda78@gmail.com>
2026-09-17 20:48:03 +0700

    upate bulan paa halaman pemesanan

--- src/components/SchoolForm.jsx
+++ src/components/SchoolForm.jsx
@@ -70,11 +70,19 @@ export default function SchoolForm({ onSubmit }) {
           onChange={handleChange}
           required
         >
-          <option value="">Pilih Rentang Anggaran</option>
-          <option value="under-10m">Di bawah Rp10 juta</option>
-          <option value="10m-50m">Rp10–50 juta</option>
-          <option value="50m-100m">Rp50–100 juta</option>
-          <option value="over-100m">Di atas Rp100 juta</option>
+          <option value="">Pilih Bulan Anggaran</option>
+          <option value="januari">Januari</option>
+          <option value="februari">Februari</option>
+          <option value="maret">Maret</option>
+          <option value="april">April</option>
+          <option value="mei">Mei</option>
+          <option value="juni">Juni</option>
+          <option value="juli">Juli</option>
+          <option value="agustus">Agustus</option>
+          <option value="september">September</option>
+          <option value="oktober">Oktober</option>
+          <option value="november">November</option>
+          <option value="desember">Desember</option>
         </select>
 
         <input
